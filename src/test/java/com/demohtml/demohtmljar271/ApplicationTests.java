805d835feafb4c907135b79f247ace6fc81ba191
Palash <parpanipalash@gmail.com>
2023-05-03 21:03:14 +0530

    adding files for demohtmljar

---
+++ src/test/java/com/demohtml/demohtmljar271/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demohtml.demohtmljar271;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
